6ea3878fe7a260b1d4ecb5e9c28c386379ba9b1c
brandonStell <brandon.stell@parisdescartes.fr>
2020-03-28 23:38:04 +0100

    20200328

--- hospitalizations.py
+++ hospitalizations.py
@@ -10,7 +10,10 @@ from scipy.optimize import curve_fit
 
 
  # define your function:
-def gauss_func(x, height, mu, sigma): return height * np.exp(-((x-mu)**2/(2*sigma)))
+def gauss_func(x, height, mu, sigma): return height * np.exp(-((x-mu)**2/(2*sigma**2)))
+
+
+def line_func(x, m, b): return (m * x) + b
 
 
 def fetch_data_from_data_dot_gouv_website():
@@ -21,7 +24,7 @@ def fetch_data_from_data_dot_gouv_website():
 
     max_saved_date = saved_data.date_de_passage.astype('datetime64').max()
     data_url = 'https://www.data.gouv.fr/fr/datasets/donnees-des-urgences-hospitalieres-et-de-sos-medecins-relatives-a-lepidemie-de-covid-19/'
-    if (max_saved_date + pd.Timedelta('3 days')) < pd.to_datetime(datetime.today().strftime('%Y-%m-%d')):
+    if (max_saved_date + pd.Timedelta('2 days')) < pd.to_datetime(datetime.today().strftime('%Y-%m-%d')):
         page = requests.get(data_url)
         # Store the contents of the website under doc
         doc = lh.fromstring(page.content)
@@ -38,6 +41,18 @@ def fetch_data_from_data_dot_gouv_website():
     return df
 
 
+def gaussian_fit_data(s):
+    data = s.dropna().values
+    data_x = s.dropna().index.astype('int').values
+    popt, pcov = curve_fit(
+        gauss_func,
+        data_x,
+        data,
+        p0=[100, pd.Timestamp('2020-03-30').value, pd.to_timedelta('2 days').value]
+    )
+    return popt[0] * np.exp(-((s.index.astype('int') - popt[1]) ** 2 / (2 * popt[2] ** 2)))
+
+
 raw = fetch_data_from_data_dot_gouv_website()
 raw.date_de_passage = raw.date_de_passage.astype('datetime64')
 raw.set_index('date_de_passage', inplace=True)
@@ -46,7 +61,7 @@ covid = pd.DataFrame()
 covid['Paris'] = raw.where(raw.sursaud_cl_age_corona == '0')\
     .where(raw.dep == '75')\
     .nbre_hospit_corona.dropna()
-covid['Marseille'] = raw.where(raw.sursaud_cl_age_corona == '0')\
+covid['Marseilles'] = raw.where(raw.sursaud_cl_age_corona == '0')\
     .where(raw.dep == '13')\
     .nbre_hospit_corona.dropna()
 covid['Bordeaux'] = raw.where(raw.sursaud_cl_age_corona == '0')\
@@ -59,20 +74,17 @@ covid['France'] = raw.where(raw.sursaud_cl_age_corona == '0').dropna().nbre_hosp
 covid = covid.reindex(pd.date_range('2-24-2020', '5-1-2020'))
 
 
-# gaussian starting guesses
-data = covid.Paris.dropna().values
-data_x = covid.dropna().index.astype('int').values
-# fit! (given that data is an array with the data to fit)
-# predictions, dunno = fit(f, [mu, sigma, height], data, x=data_x)
-popt, pcov = curve_fit(gauss_func, data_x, data,
-                       bounds=([100., pd.Timestamp('2020-03-30').value, pd.to_timedelta('2 days').value],
-                               [1000., pd.Timestamp('2020-05-30').value, pd.to_timedelta('90 days').value]))
-
-covid['Paris_fit'] = popt[2] * np.exp(-((covid.index.astype('int') - popt[0])/popt[1])**2)
+covid['Paris_fit'] = gaussian_fit_data(covid.Paris)
+covid['Marseilles_fit'] = gaussian_fit_data(covid.Marseilles)
+covid['Strasbourg_fit'] = gaussian_fit_data(covid.Strasbourg)
+covid['Bordeaux_fit'] = gaussian_fit_data(covid.Bordeaux)
+covid['France_fit'] = gaussian_fit_data(covid.France)
 
 title = "Hospitalizations per day in France"
 fig1, ax1 = plt.subplots()
-covid.plot(y=['Paris', 'Marseille', 'Strasbourg', 'Bordeaux'], ax=ax1, title=title, grid=True, figsize=(20, 15))
+covid.plot(y=['Paris', 'Marseilles', 'Strasbourg', 'Bordeaux'], ax=ax1, title=title, grid=True, figsize=(20, 15))
+covid.plot(style='k--', y=['Paris_fit', 'Bordeaux_fit', 'Strasbourg_fit', 'Marseilles_fit'], ax=ax1)
+covid.plot(style='k--', y=['France_fit', ], secondary_y=True, ax=ax1)
 covid.plot(y="France", secondary_y=True, ax=ax1, lw=4, grid=True).get_figure().savefig('hospitalizations.png')
 # covid.plot(y=["Paris_fit"], style='.', ax=ax1)
 
@@ -92,7 +104,7 @@ covid.plot(y="France", secondary_y=True, ax=ax1, lw=4, grid=True).get_figure().s
 #     .where(raw.dep == '13')\
 #     .nbre_hospit_corona.dropna()\
 #     .plot(
-#     label='Marseille',
+#     label='Marseilles',
 #     secondary_y=True,
 #     legend=True
 #     ).get_figure().savefig('hospitalizations.png')
